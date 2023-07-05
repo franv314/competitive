ddab069e249ee9d43af742cf434d6d25ec30bc37
franv314 <>
2023-07-05 12:07:27 +0200

    100 su ois_scheduling

---
+++ olinfo/OIS/scheduling/scheduling.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define SOURCE 0
+#define SINK 201
+#define MAXN 202
+
+const int inf = 1000000000;
+
+int n = MAXN;
+vector<vector<int>> capacity, flow;
+vector<int> height, excess;
+
+void push(int u, int v)
+{
+    int d = min(excess[u], capacity[u][v] - flow[u][v]);
+    flow[u][v] += d;
+    flow[v][u] -= d;
+    excess[u] -= d;
+    excess[v] += d;
+}
+
+void relabel(int u)
+{
+    int d = inf;
+    for (int i = 0; i < n; i++) {
+        if (capacity[u][i] - flow[u][i] > 0)
+            d = min(d, height[i]);
+    }
+    if (d < inf)
+        height[u] = d + 1;
+}
+
+vector<int> find_max_height_vertices(int s, int t) {
+    vector<int> max_height;
+    for (int i = 0; i < n; i++) {
+        if (i != s && i != t && excess[i] > 0) {
+            if (!max_height.empty() && height[i] > height[max_height[0]])
+                max_height.clear();
+            if (max_height.empty() || height[i] == height[max_height[0]])
+                max_height.push_back(i);
+        }
+    }
+    return max_height;
+}
+
+int max_flow(int s, int t)
+{
+    height.assign(n, 0);
+    height[s] = n;
+    flow.assign(n, vector<int>(n, 0));
+    excess.assign(n, 0);
+    excess[s] = inf;
+    for (int i = 0; i < n; i++) {
+        if (i != s)
+            push(s, i);
+    }
+
+    vector<int> current;
+    while (!(current = find_max_height_vertices(s, t)).empty()) {
+        for (int i : current) {
+            bool pushed = false;
+            for (int j = 0; j < n && excess[i]; j++) {
+                if (capacity[i][j] - flow[i][j] > 0 && height[i] == height[j] + 1) {
+                    push(i, j);
+                    pushed = true;
+                }
+            }
+            if (!pushed) {
+                relabel(i);
+                break;
+            }
+        }
+    }
+
+    int max_flow = 0;
+    for (int i = 0; i < n; i++)
+        max_flow += flow[i][t];
+    return max_flow;
+}
+
+int main() {
+    int N; cin >> N;
+    capacity.assign(MAXN, vector<int>(MAXN, 0));
+    for (int i = 1; i <= N; i++) {
+        cin >> capacity[SOURCE][i];
+    }
+    for (int i = 1; i <= N; i++) {
+        cin >> capacity[i][SINK];
+    }
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            cin >> capacity[i][j];
+        }
+    }
+    cout << max_flow(SOURCE, SINK);
+}
