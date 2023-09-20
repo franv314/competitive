3658e7c0418572209614e08e3c686b2a3b3483a5
franv314 <francesco@vercellesi.com>
2023-09-20 22:03:14 +0200

    sol veloce di corteo

--- olinfo/OII/corteo/corteo.cpp
+++ olinfo/OII/corteo/corteo.cpp
@@ -1,62 +1,91 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> adj[1000];
-vector<vector<int>> dst;
-int P1, P2, D1, D2;
+#define MAXN 1000
 
 void sposta(int chi, int dove);
 
-bool test(int threshold) {
-    bool visited[1000][1000] = {};
-    queue<pair<int, int>> q;
-
-    if (dst[P1][P2] >= threshold) q.push({P1, P2});
-    while (!q.empty()) {
-        auto [p1, p2] = q.front();
-        q.pop();
-        if (visited[p1][p2]) continue;
-        if (p1 == D1 && p2 == D2) return true;
-
-        visited[p1][p2] = true;
-        for (auto d1: adj[p1])
-            if (!visited[d1][p2] && dst[d1][p2] >= threshold)
-                q.push({d1, p2});
-        for (auto d2: adj[p2])
-            if (!visited[p1][d2] && dst[p1][d2] >= threshold)
-                q.push({p1, d2});
+struct DSU {
+    vector<int> arr;
+    
+    int find(int node) {
+        if (arr[node] < 0) return node;
+        return arr[node] = find(arr[node]);
     }
-    return false;
-}
+    
+    void join(int u, int v) {
+        u = find(u), v = find(v);
+        if (u == v) return;
+        if (arr[u] > arr[v]) swap(u, v);
+        arr[u] += arr[v];
+        arr[v] = u;
+    }
+    
+    DSU(int size) : arr(size, -1) {}  
+};
+
+int dst[MAXN][MAXN];
+int queues[MAXN][MAXN];
+int idx[MAXN];
+int q[10 * MAXN * MAXN];
+int l, r;
+int adj[MAXN][MAXN];
+int adj_idx[MAXN];
 
 int pianifica(int N, int M, int P1, int D1, int P2, int D2, int A[], int B[]) {
-    ::P1 = P1, ::P2 = P2, ::D1 = D1, ::D2 = D2;
-    dst = vector<vector<int>>(N, vector<int>(N, 1e9));
-
-    // Populate adjacency list
     for (int i = 0; i < M; i++) {
-        adj[A[i]].push_back(B[i]);
-        adj[B[i]].push_back(A[i]);
+        adj[A[i]][adj_idx[A[i]]++] = B[i];
+        adj[B[i]][adj_idx[B[i]]++] = A[i];
     }
-
-    // Populate distances
+    
     for (int i = 0; i < N; i++) {
-        queue<pair<int, int>> q;
-        
-        q.push({i, 0});
-        while (!q.empty()) {
-            auto [node, d] = q.front();
-            q.pop();
-            if (dst[i][node] != 1e9) continue;
+        int *qref = queues[i];
+        int *dref = dst[i];
+        q[r++] = i;
+        dref[i] = 1;
+        while (l != r) {
+            int x = q[l++];
             
-            dst[i][node] = d;
-            for (auto dest: adj[node])
-                if (dst[i][dest] == 1e9)
-                    q.push({dest, d + 1});
+            qref[idx[i]++] = x;
+            
+            for (int j = 0; j < adj_idx[x]; j++) {
+                int y = adj[x][j];
+                if (!dref[y]) {
+                    q[r++] = y;
+                    dref[y] = dref[x] + 1;
+                }
+            }
         }
     }
 
-    int l = 0, r = min(dst[P1][P2], dst[D1][D2]) + 1;
-    while (l + 1 < r) test((l + r) / 2) ? l = (l + r) / 2 : r = (l + r) / 2;
-    return l;
+    if (P1 == D1 && P2 == D2) {
+        return dst[D1][D2] - 1;
+    }
+    
+    DSU dsu(N * N);
+    for (int dist = dst[D1][D2]; ; dist--) {
+        for (int x = 0; x < N; x++) {
+            int *qref = queues[x];
+            while (dst[x][qref[idx[x] - 1]] >= dist) {
+                int y = qref[--idx[x]];
+                
+                for (int j = 0; j < adj_idx[x]; j++) {
+                    int x2 = adj[x][j];
+                    if (dst[x2][y] >= dist) {
+                        dsu.join(N * x + y, N * x2 + y);
+                    }
+                }
+                for (int j = 0; j < adj_idx[y]; j++) {
+                    int y2 = adj[y][j];
+                    if (dst[x][y2] >= dist) {
+                        dsu.join(N * x + y, N * x + y2);
+                    }
+                }
+            }
+        }
+        
+        if (dsu.find(P1 * N + P2) == dsu.find(D1 * N + D2)) {
+            return dist - 1;
+        }
+    }
 }
