d0d00122f972c597432b18fc8477a0ea93d6af42
franv314 <francesco@vercellesi.com>
2023-10-22 14:42:45 +0200

    corridoi improved

--- olinfo/OII/corridoi/corridoi.cpp
+++ olinfo/OII/corridoi/corridoi.cpp
@@ -1,15 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void dijkstra(int N, int S, const vector<vector<pair<int, int>>> &adj, vector<long long> &D) {
-    vector<bool> visited(N);
+#define MAXN 100'000
+
+bitset<MAXN> visited;
+vector<pair<int, int>> adj[MAXN];
+
+void dijkstra(int N, int S, vector<long long> &D) {
+    visited.reset();
     priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<>> q;
 
     q.emplace(D[S] = 0, S);
     while (!q.empty()) {
         auto [d, n] = q.top(); q.pop();
-        if (visited[n]) continue;
-        visited[n] = true;
+        if (visited.test(n)) continue;
+        visited.set(n);
         for (auto [x, w]: adj[n]) {
             if (d + w < D[x]) {
                 q.emplace(D[x] = d + w, x);
@@ -19,31 +24,25 @@ void dijkstra(int N, int S, const vector<vector<pair<int, int>>> &adj, vector<lo
 }
 
 vector<long long> shorten(int N, int M, int Q, vector<long long> K, vector<int> A, vector<int> B, vector<int> C) {
-    vector<vector<pair<int, int>>> adj(N);
     for (int i = 0; i < M; i++) {
         adj[A[i]].emplace_back(B[i], C[i]);
         adj[B[i]].emplace_back(A[i], C[i]);
     }
 
     vector<long long> D0(N, 1e18), D1(N, 1e18), D2(N, 1e18);
-    dijkstra(N, 0, adj, D0);
-    dijkstra(N, 1, adj, D1);
-    dijkstra(N, 2, adj, D2);
+    dijkstra(N, 0, D0);
+    dijkstra(N, 1, D1);
+    dijkstra(N, 2, D2);
 
-    multiset<long long> two, one;
+    long long one = 1e18;
 
-    deque<array<long long, 3>> changes;
+    vector<pair<long long, long long>> two(N + 1);
+    two[N] = {1e18, 1e18};
 
-    long long mi = 1e18;
     for (int i = 0; i < N; i++) {
-        long long common = D1[i];
-        long long tot = 2 * common + D0[i] + D2[i];
-
-        mi = min(mi, D0[i] + D1[i] + D2[i]);
-        changes.push_back({common, tot, tot - common});
-        two.insert(tot);
+        two[i] = {2 * D1[i] + D0[i] + D2[i], D1[i]};
     }
-    sort(changes.begin(), changes.end());
+    sort(two.begin(), two.end());
 
     vector<pair<long long, int>> NK(Q);
     vector<long long> ans(Q);
@@ -51,14 +50,17 @@ vector<long long> shorten(int N, int M, int Q, vector<long long> K, vector<int> 
     for (int i = 0; i < Q; i++) NK[i] = {K[i], i};
     sort(NK.begin(), NK.end());
 
+    int idx2 = 0;
     for (auto [pos, idx]: NK) {
-        if (pos >= mi) break;
-        while (!changes.empty() && changes.front()[0] <= pos) {
-            two.erase(two.find(changes.front()[1]));
-            one.insert(changes.front()[2]);
-            changes.pop_front();
+        while (two[idx2].second < pos) {
+            one = min(one, two[idx2].first - two[idx2].second);
+            idx2++;
         }
-        ans[idx] = min(two.empty() ? LLONG_MAX : *two.begin() - 2 * pos, one.empty() ? LLONG_MAX : *one.begin() - pos);
+        ans[idx] = min(two[idx2].first - 2 * pos, one - pos);
+        if (ans[idx] <= 0) {
+            ans[idx] = 0;
+            break;
+        }
     }
 
     return ans;
